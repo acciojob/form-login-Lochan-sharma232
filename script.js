7fb65536f2a544c13934922970f69ab6526dac4f
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-12 18:52:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 doocument.addEventListener('DOMContentLoaded',function() {
 document.getElementById('form1').addEventListener('submit',function(event)
 event.preventDefault();
+	function getFormvalue() {
 const fname=document.querySelector(fname).value;
 	const lname=document.querySelector(lname).value;
 const form=`${fname}+"-"+${lname}`;
 alert("<First Name> <Last Name>")
 });
 });
-	function getFormvalue() {
-	
-    //Write your code here
-
-
-
-}
+	//function getFormvalue() {
+	}
