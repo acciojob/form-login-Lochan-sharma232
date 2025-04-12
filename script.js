0bae4ab270ac06b0a75e6eb8e762d8d7ad159164
Lochan-sharma232 <lochansharma537@gmail.com>
2025-04-12 23:19:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 document.addEventListener('DOMContentLoaded',function() {
-document.getElementById('form1').addEventListener('submit',function(event)
-event.preventDefault();
-	function getFormvalue() {
-const fname=document.querySelector('input=["fname"]').value;
-	const lname=document.querySelector('input=["lname"]').value;
-const form=`${fname}+"-"+${lname}`;
-alert(form);
-	}
-	getFormvalue();
-});
-});
-	//function getFormvalue() {
+	document.getElementById('form1').addEventListener('submit',function(event){
+	event.preventDefault();
+		function getFormvalue() {
+	const fname=document.querySelector('input[name="fname"]').value;
+		const lname=document.querySelector('input[name="lname"]').value;
+	const form=`${fname}-${lname}`;
+	alert(form);
+		}
+		getFormvalue();
+	});
+	});
+	
